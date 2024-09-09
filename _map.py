4b58724b4893d11ca09c7b4313b820d30afb516b
ASPPIBRA-DEV <sandro_ceo@asppibra.com.br>
2024-09-09 18:58:06 -0300

    Update _map.py
    Adiciona gráfico interativo com Plotly e Dash Bootstrap Components
    
    - Importa as bibliotecas necessárias: Dash Core Components, Dash Bootstrap Components e Plotly Graph Objects.
    - Cria uma figura Plotly com um layout de tema escuro.
    - Configura a cor de fundo da figura para transparente.
    - Cria um componente Row do Dash Bootstrap para exibir o gráfico interativo com altura definida.
    - Atribui um ID ao gráfico para referência futura em callbacks.

--- _map.py
+++ _map.py
@@ -7,7 +7,7 @@ fig = go.Figure()
 
 fig.update_layout(template="plotly_dark", paper_bgcolor="rgba(0, 0, 0, 0)")
 
-map = dbc.Row(
-    [dcc.Graph(id="map-graph", figure=fig)],
-    style={"height": "80vh"}
+hist = dbc.Row(
+    [dcc.Graph(id="hist-graph", figure=fig)],
+    style={"height": "20vh"}
 )
