5757701a9c74417c64a50eac388cbfdd15fd614b
ASPPIBRA-DEV <sandro_ceo@asppibra.com.br>
2024-09-09 18:52:24 -0300

    Update app.py
    Configura aplicação Dash com tema Bootstrap
    
    - Inicializa uma nova instância da aplicação Dash.
    - Aplica o tema 'CYBORG' do Dash Bootstrap Components para estilização da interface.
    - Define o servidor da aplicação, facilitando a implantação.
    - Configura os scripts para serem servidos localmente, garantindo acessibilidade durante o desenvolvimento.

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import dash
 import dash_bootstrap_components as dbc
 
+
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
 server = app.server
 app.scripts.config.serve_locally = True
-
-if __name__ == "__main__":
-    app.run_server(debug=True)
-
-
+server = app.server
+ 
