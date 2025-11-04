69d8b04de2e20b573ab09afe9b3e4c6171982dad
Sandro A A Antunes <associaasppibra@gmail.com>
2025-11-04 02:59:59 +0000

    eu quero saber da grid que está usando para posiciona a tag dos cabeçari

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -36,7 +36,7 @@ const CardTitle = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "text-2xl font-semibold leading-none tracking-tight",
+      "flex items-center gap-2 text-2xl font-semibold leading-none tracking-tight",
       className
     )}
     {...props}
